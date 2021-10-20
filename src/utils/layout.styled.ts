7ec60d2dc48eeb4c5f33599e53c9b831d658e55e
oliver <oliver.wu.1987@gmail.com>
2021-10-20 13:19:10 +0000

    add layout css

--- src/utils/layout.styled.ts
+++ src/utils/layout.styled.ts
@@ -1,13 +1,36 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
+	padding-right: 0.75rem;
+	padding-left: 0.75rem;
+	margin-right: auto;
+	margin-left: auto;
+
+	@media (min-width: 576px) {
+		max-width: 540px;
+  	}
+
+	@media (min-width: 768px) {
+		max-width: 720px;
+  	}
+	@media (min-width: 992px) {
+		max-width: 960px;
+	}
+	@media (min-width: 1200px) {
+		max-width: 1116px;
+	}
 
 `;
 
 export const Row = styled.div`
+	margin-right: -0.75rem;
+	margin-left: -0.75rem;
 
 `;
 
 export const Col = styled.div`
-
+	position: relative;
+	width: 100%;
+	padding-right: 0.75rem;
+	padding-left: 0.75rem;
 `;
